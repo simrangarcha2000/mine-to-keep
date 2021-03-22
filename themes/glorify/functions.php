760f746fc0cf1039dbb79dd6793e9ef856c4cd6f
Simran Garcha <simranjeetkaur.garcha@dcmail.ca>
2021-03-21 22:52:08 -0400

    Creating separate styles for WooCommerce

--- themes/glorify/functions.php
+++ themes/glorify/functions.php
@@ -192,12 +192,12 @@ function glorify_scripts() {
 	//Foundation (should always be below main style sheet and custom  style sheet after)
 	wp_enqueue_style('foundation-style', get_template_directory_uri().'/assets/css/vendor/foundation.css');
 
-	wp_enqueue_style('custom-style', get_template_directory_uri().'/assets/css/style.css');
-
-	wp_enqueue_style('woocommerce-style', get_template_directory_uri().'/assets/css/woocommerce.css');
-
 	//To put the script to the footer following the syntax and putting footer as true
 	wp_enqueue_script('foundation-script', get_template_directory_uri().'/assets/js/vendor/foundation.js', array(), false, true);
+
+	wp_enqueue_style('custom-style', get_template_directory_uri().'/assets/css/custom.css');
+
+	wp_enqueue_style('woocommerce-style', get_template_directory_uri().'/assets/css/woocommerce.css');
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
